55c84d7f7587d7b4dd177e7042a6809b2cae5781
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2024-02-07 17:58:49 +0100

    Modifica view Index e admin

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -7,18 +7,30 @@
                 @foreach ($events as $event)
                     <div class="col">
                         <div class="card" style="width: 18rem;">
-                            <img src="{{$event->img}}" class="card-img-top" alt="{{$event->name}}">
+                            <img src="{{ $event->img }}" class="card-img-top" alt="{{ $event->name }}">
                             <div class="card-body">
-                                <h5 class="card-title"><strong>{{$event->name}}</strong> </h5>
-                                <p class="card-text">{{$event->description}}</p>
+                                <h5 class="card-title"><strong>{{ $event->name }}</strong> </h5>
+                                <p class="card-text">{{ $event->description }}</p>
+                                <p class="card-text"> <strong>Responsabile dell'evento:</strong>
+                                    {{ $event->user ? $event->user->name : 'Nessuno' }}</p>
                                 <div class="card-footer mb-3">
-                                    <small class="text-body-secondary">Data: {{$event->date}}</small>
-                                  </div>
-                                 
+                                    <small class="text-body-secondary">Data: {{ $event->date }}</small>
+                                </div>
+                            </div>
+                            <div class="card-subtitle mb-2 text-muted pt-2">
+                                @if (count($event->tags) > 0)
+                                    <ul> Tipo evento:
+                                        @foreach ($event->tags as $tag)
+                                            <li> {{ $tag->name }}</li>
+                                        @endforeach
+                                    </ul>
+                                @else
+                                    <p>No Tag #</p>
+                                @endif
                             </div>
                             <a href="{{ route('admin.events.show', $event->id) }}" class="btn btn-primary">Dettagli</a>
                             <a href="{{ route('admin.events.edit', $event->id) }}" class="btn btn-warning">Modifica</a>
-                            <form action="{{ route('admin.events.destroy',$event->id) }}" method="POST">
+                            <form action="{{ route('admin.events.destroy', $event->id) }}" method="POST">
                                 @csrf
                                 @method('DELETE')
                                 <input type="submit" value="Cancella" class="btn btn-danger">
@@ -29,5 +41,4 @@
             </div>
         </div>
     </div>
-
 @endsection
